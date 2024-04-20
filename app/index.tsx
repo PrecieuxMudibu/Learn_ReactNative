6150512259488acbdd8f51dd1e6f18703e8be834
PrecieuxMudibu <oceangpmb@gmail.com>
2024-04-20 20:56:31 +0100

    feat: Customize the header

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,43 @@
-import { View, Text } from "react-native"
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView, Text } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { images, icons, COLORS, FONT, SIZES, SHADOWS } from '@/constants';
+import {
+    Nearbyjobs,
+    Popularjobs,
+    ScreenHeaderBtn,
+    Welcome
+} from '@/components';
 
 export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            {/* <Text>Home</Text> */}
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={images.profile}
+                            dimension="100%"
+                        />
+                    ),
+                    headerTitle: ''
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ flex: 1, padding: SIZES.medium }}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
